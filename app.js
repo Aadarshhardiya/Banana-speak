b50c6d2f761902e43b59be4c9b17a076a8bd14ef
aadarshhardiya <aadarsh.hardiya@gmail.com>
2022-08-23 23:35:03 +0530

    Final touch complete

--- app.js
+++ app.js
@@ -6,8 +6,25 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
-function clickHandler() {
-    outputDiv.innerText = "translated: " + txtInput.value
+var serverURL ="https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(text) {
+    return serverURL + "?" + "text=" + text
 }
+
+function clickHandler() {
+    var inputText = txtInput.value;
+   
+
+    var finalUrl = getTranslationURL(inputText);
+    console.log('fdfdfdfdfd-----',finalUrl)
+
+    fetch(getTranslationURL(inputText))
+    .then(response => response.json())
+    .then(json => {
+        var translatedText = json.contents.translated;
+        outputDiv.innerText = translatedText;
+    })
+};
 
 btnTranslate.addEventListener("click", clickHandler)
